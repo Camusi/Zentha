9d984a08c5e52a5e53f8bdfaae975c343f9b74e2
Sammy <scu39@uclive.ac.nz>
2025-10-02 23:48:57 +1300

    db up server down issues

--- src/main/java/com/zentha/zentha/config/EnvPropertiesInitializer.java
+++ src/main/java/com/zentha/zentha/config/EnvPropertiesInitializer.java
@@ -13,6 +13,7 @@ public class EnvPropertiesInitializer implements ApplicationContextInitializer<C
     @Override
     public void initialize(ConfigurableApplicationContext context) {
         Dotenv dotenv = Dotenv.configure()
+                .directory("Zentha")
                 .filename("env.properties")
                 .ignoreIfMissing()
                 .load();

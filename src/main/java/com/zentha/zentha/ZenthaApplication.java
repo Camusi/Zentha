4995a19a5e7c22ca4a144e50d6f353200f7235b3
Sammy <scu39@uclive.ac.nz>
2025-10-03 00:15:15 +1300

    db up server down issues

--- src/main/java/com/zentha/zentha/ZenthaApplication.java
+++ src/main/java/com/zentha/zentha/ZenthaApplication.java
@@ -1,6 +1,5 @@
 package com.zentha.zentha;
 
-import com.zentha.zentha.config.EnvPropertiesInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ public class ZenthaApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ZenthaApplication.class);
-        app.addInitializers(new EnvPropertiesInitializer());
         app.run(args);
     }
 }

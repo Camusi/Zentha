ca8675bc50ab301ec69d855847178b08000dba4f
Sammy <scu39@uclive.ac.nz>
2025-10-02 22:34:54 +1300

    local and prod setups

--- src/main/java/com/zentha/zentha/ZenthaApplication.java
+++ src/main/java/com/zentha/zentha/ZenthaApplication.java
@@ -1,5 +1,6 @@
 package com.zentha.zentha;
 
+import com.zentha.zentha.config.EnvPropertiesInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZenthaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZenthaApplication.class, args);
+        SpringApplication app = new SpringApplication(ZenthaApplication.class);
+        app.addInitializers(new EnvPropertiesInitializer());
+        app.run(args);
     }
-
 }
---
+++ src/main/java/com/zentha/zentha/config/EnvPropertiesInitializer.java
@@ -0,0 +1,26 @@
+package com.zentha.zentha.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnvPropertiesInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext context) {
+        Dotenv dotenv = Dotenv.configure()
+                .filename("env.properties")
+                .ignoreIfMissing()
+                .load();
+
+        Map<String, Object> props = new HashMap<>();
+        dotenv.entries().forEach(entry -> props.put(entry.getKey(), entry.getValue()));
+
+        context.getEnvironment().getPropertySources()
+                .addFirst(new MapPropertySource("dotenvProps", props));
+    }
+}
